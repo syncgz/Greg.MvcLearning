0ffab8dd2f09da0b0de4b97c4c66d811efc7925c
syncgz <sync.gz@gmail.com>
2012-10-11 15:04:10 +0200

    Solution start-up.

---
+++ Greg.MvcLearning.MvcTestApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Greg.MvcLearning.MvcTestApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
