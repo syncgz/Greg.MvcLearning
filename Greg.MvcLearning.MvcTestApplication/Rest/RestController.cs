8518b1308361c010382572c4311a363e3c7e8991
greg_notebook <sync.gz@gmail.com>
2012-10-14 20:27:34 +0200

    Modifications

---
+++ Greg.MvcLearning.MvcTestApplication/Rest/RestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Greg.MvcLearning.MvcTestApplication.Rest
+{
+    public class RestController : ApiController
+    {
+        [HttpGet]
+        public IQueryable<Auction> GetMethod()
+        {
+            var auctionList = new List<Auction>()
+                {
+                    new Auction() {Id = 1},
+                    new Auction() {Id = 2},
+                    new Auction() {Id = 3},
+                    new Auction() {Id = 4},
+                    new Auction() {Id = 5},
+                };
+
+            return auctionList.AsQueryable();
+        }
+
+        [HttpPut]
+        public void PutMethod()
+        {
+            
+        }
+
+        [HttpPost]
+        public void PostMethod()
+        {
+            
+        }
+
+        [HttpDelete]
+        public void DeleteMethod()
+        {
+            
+        }
+    }
+}
