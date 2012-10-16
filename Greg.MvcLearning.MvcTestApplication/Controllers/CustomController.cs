164a2834a75fb7af1857fb9d55d59ad5c3a0bd06
syncgz <sync.gz@gmail.com>
2012-10-16 13:05:35 +0200

    Add free layout + add html extension method

---
+++ Greg.MvcLearning.MvcTestApplication/Controllers/CustomController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Greg.MvcLearning.MvcTestApplication.Controllers
+{
+    public class CustomController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Index2")]
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Greg.MvcLearning.MvcTestApplication/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Greg.MvcLearning.MvcTestApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+        [ActionName("Index")]
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult WhateverNameYouWant()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+    }
+}
