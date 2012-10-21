4f81bed487b028c96678de6a8e691d2c3ecf33f5
greg_notebook <sync.gz@gmail.com>
2012-10-21 18:26:24 +0200

    Commit.

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Ninject;
+using Ninject.Syntax;
+using SportsStore.Domain.Abstract;
+
+public class NinjectDependencyResolver : IDependencyResolver
+{
+    private IKernel kernel;
+
+    public NinjectDependencyResolver()
+    {
+        kernel = new StandardKernel();
+        AddBindings();
+    }
+
+    // Funkcja implementujaca interface
+    public object GetService(Type serviceType)
+    {
+        return kernel.TryGet(serviceType);
+    }
+
+    // Funkcja implementujaca interface
+    public IEnumerable<object> GetServices(Type serviceType)
+    {
+        return kernel.GetAll(serviceType);
+    }
+
+    // Funkcja pomocnicza by z zewnatrz mozna bylo dodawac nowe bindingi
+    public IBindingToSyntax<T> Bind<T>()
+    {
+        return kernel.Bind<T>();
+    }
+    
+    // Publiczna instancja resolvera ninjectowego
+    public IKernel Kernel
+    {
+        get { return kernel; }
+    }
+
+    // Inicializacja ninjecta odpowiednimi wiazaniami miedzy interfacami a klasami implementujacymi
+    private void AddBindings()
+    {
+        //
+        //
+        // Funkcja rejestrujaca odpowiednie dependences!!!!!!! E.g.
+        //
+        //
+        //// put additional bindings here 
+        //Bind<IProductRepository>().To<EFProductRepository>();
+
+        //// create the email settings object 
+        //EmailSettings emailSettings = new EmailSettings
+        //{
+        //    WriteAsFile = bool.Parse(
+        //    ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+        //};
+        
+        //Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
+    }
+}
---
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UrlsAndRoutes
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            // default: 
+            // http://localhost:9006/home/index
+            // http://localhost:9006/home
+            // http://localhost:9006
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            // sciezka statyczna: 
+            // http://localhost:9006/test/home/index
+            routes.MapRoute(
+                name: "Default1",
+                url: "Test/{controller}/{action}");
+        }
+    }
+}
