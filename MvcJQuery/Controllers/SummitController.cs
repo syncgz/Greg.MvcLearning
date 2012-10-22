7e938868fee26c2dd52010482e44f0448681f7d9
greg_notebook <sync.gz@gmail.com>
2012-10-22 23:23:19 +0200

    jQuery

---
+++ MvcJQuery/Controllers/SummitController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using MvcJQuery.Models;
+
+namespace MvcJQuery.Controllers
+{
+    public class SummitController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Summit> list = new List<Summit>()
+                {
+                    new Summit(){Height = 1000,Name = "A"},
+                    new Summit(){Height = 2000,Name = "B"},
+                    new Summit(){Height = 3000,Name = "C"},
+                    new Summit(){Height = 4000,Name = "D"}
+
+                };
+
+            return View(list);
+        }
+
+        public ActionResult AddSummit()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteSummit()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcJQuery/Models/Summit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcJQuery.Models
+{
+    public class Summit
+    {
+        public String Name { get; set; }
+
+        public int Height { get; set; }
+    }
+}
