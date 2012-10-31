1b8be5712f4527bc88da275bdfc5dbe519c6f9d8
syncgz <sync.gz@gmail.com>
2012-10-31 17:03:53 +0100

    Commit

--- AjaxMvc2/Controllers/CustomAjax2Controller.cs
+++ AjaxMvc2/Controllers/CustomAjax2Controller.cs
@@ -6,6 +6,11 @@ using System.Web.Mvc;
 
 namespace AjaxMvc2.Controllers
 {
+    // Kontroler obslugujacy czesciowe odswiezanie strony czyli akcja
+    //
+    //Browser request z parametami --> Controller --> Browser
+    //
+
     public class CustomAjax2Controller : Controller
     {
         private static List<string> _comments = new List<string>();
